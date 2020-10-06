5d5b3ae440afaefb57a4cfe09b2a59aaff7995b0
KHANFARA <KHANFARA@KKHANFARALW1CMA>
2020-10-06 11:11:43 +0100

    bitcoin price

--- Axios Application/index.js
+++ Axios Application/index.js
@@ -2,12 +2,13 @@ new Vue({
     el: '#app',
     data () {
       return {
-        info: null
+        info: null,
+        data : null
       }
     },
     mounted () {
       axios
         .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-        .then(response => (this.info = response))
+        .then(response => (this.data = response.data))
     }
   })
